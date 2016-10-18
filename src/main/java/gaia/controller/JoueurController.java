ffca05b1734fd969c84ea80f2facf821dc5a5e7e
tmpUsername <admin@fddf-PC>
2016-10-18 10:08:41 +0200

    JoueurController.java: ajout du service

--- src/main/java/gaia/controller/JoueurController.java
+++ src/main/java/gaia/controller/JoueurController.java
@@ -5,6 +5,8 @@
  */
 package gaia.controller;
 
+import gaia.service.JoueurServiceCRUD;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,8 +18,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class JoueurController {
     
+    @Autowired
+    private JoueurServiceCRUD service;
+    
     @RequestMapping(value = "/", method = RequestMethod.GET)
         public String home() {
+            
+            
             return "home.jsp";
         }
 }
