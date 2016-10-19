50c6219e0a6fd974b42dd4ed4f0fb62ea3e9f7cf
tmpUsername <julien.duthoit.dev@gmail.com>
2016-10-19 11:49:45 +0200

    LuneService.java: ajout d'une classe d'avancement du temps

---
+++ src/main/java/gaia/service/LuneService.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gaia.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class LuneService {
+ 
+    private long lune=0;
+    
+    @Scheduled(fixedDelay = 600000)
+    public void inscrementeLune(){
+        
+        System.out.println( "*** LUNE : " + lune );
+        lune++;
+    }
+}
