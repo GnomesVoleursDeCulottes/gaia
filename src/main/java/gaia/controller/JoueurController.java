c42376f4b0e1a29b3754fa1ec3918db06ce7f4d6
Nordine59 <itounour@hotmail.fr>
2016-10-19 12:16:34 +0200

    Fusion modif laurent et modif nordine

--- src/main/java/gaia/controller/JoueurController.java
+++ src/main/java/gaia/controller/JoueurController.java
@@ -5,25 +5,51 @@
  */
 package gaia.controller;
 
+import gaia.entity.Joueur;
 import gaia.service.JoueurServiceCRUD;
+import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
+/*
  *
  * @author admin
  */
 @Controller
 public class JoueurController {
-    
+
     @Autowired
     private JoueurServiceCRUD service;
-    
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-        public String home() {
-            service.findAll();
-            return "index.jsp";
+    public String ajouterGET(Model model) {
+        model.addAttribute("JoueurAttr", new Joueur());
+        return "index.jsp";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String ajouterMdp(@ModelAttribute("nouveauJoueur") Joueur joueur, HttpSession s) {
+
+        Joueur leJoueur = service.findOneByLogin(joueur.getLogin());
+        if (leJoueur == null){
+            
+            //ajouter valeur de départ dépendant des cycles
+            leJoueur = joueur;
+            service.save(leJoueur);
+            
         }
+           
+        s.setAttribute("idUser", leJoueur.getId());
+        
+        return "index.jsp";
+    }
+    
+    @RequestMapping(value = "/jeu", method = RequestMethod.GET)
+    public String jeu(){
+        return "gaiaDashboard.jsp";
+    }
 }
