ef805f5faf955374d7af89b2ebfacd8278b30dfc
tmpUsername <julien.duthoit.dev@gmail.com>
2016-10-19 16:12:15 +0200

    LuneService: ajout de la gestion du cycle de vie

--- src/main/java/gaia/service/LuneService.java
+++ src/main/java/gaia/service/LuneService.java
@@ -5,6 +5,10 @@
  */
 package gaia.service;
 
+import gaia.entity.Chevre;
+import gaia.entity.Joueur;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -14,19 +18,73 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class LuneService {
- 
-    private long lune=0;
-    
+
+    private long lune = 0;
+    @Autowired
+    private JoueurServiceCRUD service;
+    @Autowired
+    private ChevreServiceCRUD serviceChevre;
+
     @Scheduled(fixedDelay = 300000)
-    public void incrementeLune(){
-        
-        System.out.println( "*** LUNE : " + lune );
+    public void incrementeLune() {
+
+        System.out.println("*** FIN LUNE : " + (lune - 1L) + " ***");
+        cycleDeVie();
+        System.out.println("*** CYCLE LUNE : " + lune + " OK ***");
         lune++;
+        
     }
 
     public long getLune() {
         return lune;
     }
-    
-    
+
+    private void cycleDeVie() {
+        //parcours tous les joueurs
+        List<Joueur> lesJoueurs = service.findAll();
+        for (Joueur joueur: lesJoueurs){
+            ////Mort
+            //Fin de partie
+            if (joueur.getProchainRepas() < lune){
+                ////?????
+            }
+            //Mort chévre + fromage
+            for (Chevre chevre: joueur.getChevres()){
+                if (chevre.getProchainRepas() < lune){
+                    serviceChevre.delete(chevre);
+                } else {
+                    if (chevre.getProchainFromage() == lune){
+                        chevre.setProchainFromage(lune + 6L);
+                        joueur.setQuantiteFromage(joueur.getQuantiteFromage() + 2L + (long)Math.floor(Math.random()*2));
+                    }
+                }
+            }
+            ////Naissance (sauf fromage)
+            //Recolte Blé
+            if (joueur.getBlePlante().containsKey(lune)){
+                joueur.setQuantiteBle(joueur.getQuantiteBle() + joueur.getBlePlante().get(lune));
+                joueur.getBlePlante().remove(lune);
+            }
+            
+            //Recolte Carotte
+            if (joueur.getCarottePlantee().containsKey(lune)){
+                joueur.setQuantiteCarotte(joueur.getQuantiteCarotte()+ joueur.getCarottePlantee().get(lune));
+                joueur.getCarottePlantee().remove(lune);
+            }
+            //Maturation Chevraux
+            if (joueur.getChevraux().containsKey(lune)){
+                for (long i = 0L; i < joueur.getChevraux().get(lune); i++){
+                    Chevre nouvelleChevre = new Chevre();
+                    nouvelleChevre.setLeJoueur(joueur);
+                    nouvelleChevre.setProchainFromage(lune + 6L);
+                    nouvelleChevre.setProchainRepas(lune + 4L);
+                    nouvelleChevre.setProchaineGestation(lune);
+                    serviceChevre.save(nouvelleChevre);
+                    joueur.getChevres().add(nouvelleChevre);
+                }
+                joueur.getChevraux().remove(lune);
+            }
+            service.save(joueur);
+        }
+    }
 }
