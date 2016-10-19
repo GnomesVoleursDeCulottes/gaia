8cce4c58c04b40d00f6d8818f0cf66b047b45206
Nordine59 <itounour@hotmail.fr>
2016-10-19 12:36:32 +0200

    ChevreServiceCRUD.java: correction erreur

--- src/main/java/gaia/service/ChevreServiceCRUD.java
+++ src/main/java/gaia/service/ChevreServiceCRUD.java
@@ -15,6 +15,5 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface ChevreServiceCRUD extends CrudRepository<Chevre, Long>{
     
-    public List<Chevre> findAllByIdOrderByIdDesc();
     
 }
