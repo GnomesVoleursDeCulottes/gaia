9df13e86ea2137dce072bfb8325b6af32fc828ea
tmpUsername <julien.duthoit.dev@gmail.com>
2016-10-20 23:42:13 +0200

    gaia.js: ajout d'une fonction pour planter le blé

--- src/main/webapp/JS/gaia.js
+++ src/main/webapp/JS/gaia.js
@@ -1,7 +1,16 @@
+var planterBle = function(ble){
+    var nbPlante;
+    do {
+        nbPlante = prompt("Combien de blé voulez vous planter? (max " + ble + ")");
+    }while (nbPlante < 0 || nbPlante > ble);
+    var tmp ="/gaia/planterBle/" + nbPlante ;
+    console.log(tmp);
+    $.post(tmp);
+};
+
 var refresh = function () {
     $("#stock").load("/gaia/ressource");
     $("#cycle").load("/gaia/cycle");
 };
 
-
-setInterval(refresh, 1500);
+setInterval(refresh, 1000);
