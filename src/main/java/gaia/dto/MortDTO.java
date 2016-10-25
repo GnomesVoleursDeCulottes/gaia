d2ceeade5e7f55c4e7c06e08b9e46d60b1db32e1
fantay <laurentcoll@hotmail.com>
2016-10-25 14:22:23 +0200

    mortDTO.java creation du fichier dto

---
+++ src/main/java/gaia/dto/MortDTO.java
@@ -0,0 +1,25 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gaia.dto;
+
+/**
+ *
+ * @author Laurent-LIM
+ */
+public class MortDTO {
+    
+    private boolean mort=false;
+
+    public boolean isMort() {
+        return mort;
+    }
+
+    public void setMort(boolean mort) {
+        this.mort = mort;
+    }
+    
+    
+}
